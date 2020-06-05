50b654c40be4422793e012a7301348618bf462c1
daro <dario.hruszecki@gmail.com>
2020-06-05 19:13:59 -0300

    hitos, commit inicial

---
+++ Hitos.R
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: daro
+# Created on: 5/6/20
+
+library(here)
+library(ggplot2)
+library(dplyr)
+
+
+hitos <- read.csv(here("cdatos-DM-TP1", "resources" , "hitos_discretizado.csv"))
